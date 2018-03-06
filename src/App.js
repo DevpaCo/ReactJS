6969561a5679c8f2e526082d431fe312fc698c25
Santi Teeragul <santitee@gmail.com>
2018-03-06 18:19:13 +0700

    CREATED `UI Added layout`

--- src/App.js
+++ src/App.js
@@ -2,7 +2,85 @@ import React, { Component } from 'react';
 
 export default class App extends Component {
 	render() {
-		return 'Devpa Co. Practice on ReactJS with Stateful Component';
+		return (
+			<div className='root'>
+				<div>
+					<div>
+						<div>
+							<h2>ROHASO MESSENGER SERVICE</h2>
+							<h2>Deliver your items quickly and safely</h2>
+						</div>
+					</div>
+				</div>
+				<div>
+					<div>
+						<div>
+							<h2>SENDER</h2>
+						</div>
+						<div>
+							<h2>Tap to search addresses</h2>
+						</div>
+					</div>
+					<div>
+						<div>
+							<h2>RECENT</h2>
+						</div>
+						<div>
+							<h2>{">"}</h2>
+						</div>
+					</div>
+				</div>
+				<div>
+					<div>
+						<div>
+							<h2>RECIPIENT</h2>
+						</div>
+						<div>
+							<h2>Tap to search addresses</h2>
+						</div>
+					</div>
+					<div>
+						<div>
+							<h2>RECENT</h2>
+						</div>
+						<div>
+							<h2>{">"}</h2>
+						</div>
+					</div>
+				</div>
+				<div>
+					<div>
+						<div>
+							<h2>NOTES TO DRIVER</h2>
+						</div>
+						<div>
+							<h2>Specify order details</h2>
+						</div>
+					</div>
+					<div>
+						<div>
+							<h2>{">"}</h2>
+						</div>
+					</div>
+				</div>
+				<div>
+					<div>
+						<div>
+							<h2>ROUND-TRIP</h2>
+						</div>
+						<div>
+							<h2>Set as round-trip</h2>
+						</div>
+					</div>
+				</div>
+				<div>
+					<div>
+						<div>
+							<h2>NEXT</h2>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
 	}
 };
-
